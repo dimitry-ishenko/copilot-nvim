60104e4bfa58d5c0b58b525fdacd12e9310ec164
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-11 20:56:33 -0400

    copilot.lua: disable by default

--- debian/copilot.lua
+++ debian/copilot.lua
@@ -2,3 +2,13 @@ require("copilot").setup({
     suggestion = {enabled = false},
     panel = {enabled = false}
 })
+
+-- Disable by default
+require("copilot.command").disable()
+
+local function map(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, {noremap = true})
+end
+
+map("n", "<leader>pd", ":Copilot disable<cr>")
+map("n", "<leader>pe", ":Copilot enable<cr>")
